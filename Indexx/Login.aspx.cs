cf507265791ef842c6f64629f8ba6a2cf7c50981
cevc96 <cesar.villarrreal@gmail.com>
2016-09-26 00:59:26 -0500

    commit 2

--- Indexx/Login.aspx.cs
+++ Indexx/Login.aspx.cs
@@ -8,7 +8,7 @@ using System.Web.UI.WebControls;
 namespace Indexx
 {
     public partial class Login : System.Web.UI.Page
-    {
+    { 
         protected void Page_Load(object sender, EventArgs e)
         {
 

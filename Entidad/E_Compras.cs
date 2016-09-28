5fc87a6677ad18883c64479ad5e3e36d805e9d77
cevc96 <cesar.villarrreal@gmail.com>
2016-09-27 19:56:40 -0500

    commi orden compra

---
+++ Entidad/E_Compras.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidad
+{
+    class E_Compras
+    {
+        private int IdCompras;
+
+        public int IdCompras1
+        {
+            get { return IdCompras; }
+            set { IdCompras = value; }
+        }
+
+        private int Cantidad;
+
+        public int Cantidad1
+        {
+            get { return Cantidad; }
+            set { Cantidad = value; }
+        }
+
+        private double PrecioCan;
+
+        public double PrecioCan1
+        {
+            get { return PrecioCan; }
+            set { PrecioCan = value; }
+        }
+
+        private double PreTotal;
+
+        public double PreTotal1
+        {
+            get { return PreTotal; }
+            set { PreTotal = value; }
+        }
+
+        private double Total;
+
+        public double Total1
+        {
+            get { return Total; }
+            set { Total = value; }
+        }
+
+        private char Estado;
+
+        public char Estado1
+        {
+            get { return Estado; }
+            set { Estado = value; }
+        }
+
+        private int IdCotizacion;
+
+        public int IdCotizacion1
+        {
+            get { return IdCotizacion; }
+            set { IdCotizacion = value; }
+        }
+
+    }
+}
--- Indexx/pages/Adquisicion/WF_OrdenCompra.ascx
+++ Indexx/pages/Adquisicion/WF_OrdenCompra.ascx
@@ -1,1 +1,4 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="WF_OrdenCompra.ascx.cs" Inherits="Indexx.pages.WF_OrdenCompra" %>
+
+
+<asp:TextBox ID="txtniombre" runat="server"></asp:TextBox>

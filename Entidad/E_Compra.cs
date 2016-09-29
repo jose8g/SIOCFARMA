fd0381497b02bb27aaedb85cb2f2ec8adbf6bd29
cevc96 <cesar.villarrreal@gmail.com>
2016-09-28 23:47:01 -0500

    Elements

---
+++ Entidad/E_Compra.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidad
+{
+    class E_Compra
+    {
+        private int IdCompras;
+
+        public int IdCompras1
+        {
+            get { return IdCompras; }
+            set { IdCompras = value; }
+        }
+
+        private int Cantidad;
+
+        public int Cantidad1
+        {
+            get { return Cantidad; }
+            set { Cantidad = value; }
+        }
+
+        private double PrecioCan;
+
+        public double PrecioCan1
+        {
+            get { return PrecioCan; }
+            set { PrecioCan = value; }
+        }
+
+        private double Descuento;
+
+        public double Descuento1
+        {
+            get { return Descuento; }
+            set { Descuento = value; }
+        }
+
+        private double Total;
+
+        public double Total1
+        {
+            get { return Total; }
+            set { Total = value; }
+        }
+
+        private char Estado;
+
+        public char Estado1
+        {
+            get { return Estado; }
+            set { Estado = value; }
+        }
+
+        private int IdCotizacion;
+
+        public int IdCotizacion1
+        {
+            get { return IdCotizacion; }
+            set { IdCotizacion = value; }
+        }
+        //COMENTARIO
+    }
+}

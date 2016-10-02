b6c3d4029e4bfe438c0dc8e80a2fdeacd636a462
loayzacampos <tiogilberto96@gmail,com>
2016-10-02 13:49:45 +0200

    commit

---
+++ DAO/DAO_Item.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using Entidad;
+
+namespace DAO
+{
+    public class DAO_Item
+    {
+        private SqlCommand mCm;
+        private SqlDataAdapter mDa;
+        private DataSet mDs; 
+        SqlConnection conexion;
+        public DAO_Item()
+        {
+            conexion = new SqlConnection(ConexionBD.CadenaConexion);
+        }
+
+        public DataTable getItemsByNombre(string Nombre)
+        {
+            try
+            {
+                mDa = new SqlDataAdapter("sp_obtenerItemPorNombre", conexion);
+                mDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+                mDa.SelectCommand.Parameters.AddWithValue("@Nombre", Nombre);
+                mDs = new DataSet();
+                mDa.Fill(mDs);
+                return mDs.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+    }
+}

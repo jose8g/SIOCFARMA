c354c8ddd1e5385fa70951ba9735c228167c7bfb
jhonataniberico <jhonataniberico@gmail.com>
2016-10-26 22:30:34 -0500

    .

--- Indexx/pages/Ventas/WF_Pedidos.ascx.cs
+++ Indexx/pages/Ventas/WF_Pedidos.ascx.cs
@@ -4,54 +4,97 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using DAO;
 
 namespace Indexx.pages.Ventas
 {
     public partial class WF_Pedidos : System.Web.UI.UserControl
     {
+        DAO_Item obj;
         protected void Page_Load(object sender, EventArgs e)
         {
-            mostrarPendientes();
+            obj = new DAO_Item();
+            if (!Page.IsPostBack)
+            {
+                getItemxStock();
+            }
         }
-
-
-        protected void mostrarPendientes()
-        {
-            DAO.DAO_Pedido obj = new DAO.DAO_Pedido();
-            gridMostrarPendientes.DataSource=obj.ConsultarPedidosPendientes();
-            gridMostrarPendientes.DataBind();
-            
-        }
-
-        protected void btnRefrescar_Click(object sender, EventArgs e)
-        {
-            mostrarPendientes();
-        }
-
-
-
-
-
         protected void gvItems_PageIndexChanging(object sender, GridViewPageEventArgs e)
         {
             dgvItems.PageIndex = e.NewPageIndex;
+            dgvPedidos.PageIndex = e.NewPageIndex;
         }
 
 
-        protected void gvItems_RowComand(object sender, GridViewCommandEventArgs e)
+        protected void getItemxStock()
         {
-
-        }
-
-        protected void btnNombre_Click(object sender, EventArgs e)
-        {
-            DAO.DAO_Item obj = new DAO.DAO_Item();
-            dgvItems.DataSource = obj.getItemsByNombre(txtNombre.Value);
+            dgvItems.DataSource = obj.getItemxStock();
             dgvItems.DataBind();
         }
 
+        protected void getItemsByNombre(object sender, EventArgs e)
+        {
+            dgvItems.DataSource = obj.getItemsByNombre(txtBuscarItems.Value);
+            dgvItems.DataBind();
+        }
 
+        protected void gvItems_RowComand(object sender, GridViewCommandEventArgs e)
+        {
+            try
+            {
+                if (e.CommandName == "agregarItemxStock")
+                {
+                    int idItem = Convert.ToInt32(dgvItems.DataKeys[Convert.ToInt32(e.CommandArgument)].Values["IdItem"].ToString());
+                    //string nombre = (dgvItems.DataKeys[Convert.ToInt32(e.CommandArgument)].Values["Nombre"].ToString());
+                    //double precioVenta = Convert.ToDouble(dgvItems.DataKeys[Convert.ToInt32(e.CommandArgument)].Values["PrecioVenta"].ToString());
 
+                    //IdItemtext.Text = idItem.ToString();
+                    //Nombretxt.Text = nombre;
+                    //Preciotxt.Text = precioVenta.ToString();
 
+                    dgvPedidos.DataSource = obj.getCopyPedidoxItem();
+                    dgvPedidos.DataBind();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+            }
+        }
+
+        protected void gvPedidos_RowComand(object sender, GridViewCommandEventArgs e)
+        {
+            
+        }
+
+        protected void GuardarPedidosxItem(object sender, EventArgs e)
+        {
+            try
+            {
+                int idItem = Convert.ToInt32(IdItemtext.Text);
+                int cantidad = Convert.ToInt32(Cantidadtxt.Text);
+
+                if (Session["idPedido"] == null)
+                {
+                    int idPedido = obj.insertarPedido();
+                    obj.insertarPedidoxItem(idPedido.ToString(), idItem.ToString(), cantidad.ToString());
+                    Session["idPedido"] = idPedido;
+                }
+                else
+                {
+                    obj.insertarPedidoxItem(Session["idPedido"].ToString(), idItem.ToString(), cantidad.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+            }
+        }
+        protected void registrarPedidosxItem(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
